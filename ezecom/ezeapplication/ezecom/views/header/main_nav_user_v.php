ce77222ac8f5c35f32ce76d7bd663b2377c2b83c
ezecomtest <32106406+ezecomtest@users.noreply.github.com>
2018-02-12 22:47:29 +0700

    update menu
    update menu

--- ezecom/ezeapplication/ezecom/views/header/main_nav_user_v.php
+++ ezecom/ezeapplication/ezecom/views/header/main_nav_user_v.php
@@ -1,5 +1,37 @@
-<!-- Main Navigation -->
-<link href="<?php echo base_url()?>assets/css/navigation_menu.css" rel="stylesheet">
+<!-- Main Navigation import CSS-->
+<link href="<?php echo base_url()?>assets/css/css_v3/navigation_menu.css" rel="stylesheet">
+<!-- Main Navigation import JS-->
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/js_v3/nav_dropdown_menu.js"></script>
+
 <div class="container-fluid" style="background-color:#BCBDC0">
-	<h3>Hello</h3>
+	<div class="container">
+		<nav id='cssmenu'>
+		<div id="head-mobile"></div>
+		<div class="button"></div>
+		<ul>
+			<li class='active'><a href='#'>Home</a></li>
+			<li><a href='#'>Our Company</a></li>
+			<li><a href='#'>Our Services</a>
+			   <ul>
+				  <li><a href='#'>Product 1</a>
+					 <ul>
+						<li><a href='#'>Sub Product</a></li>
+						<li><a href='#'>Sub Product</a></li>
+					 </ul>
+				  </li>
+				  <li><a href='#'>Product 2</a>
+					 <ul>
+						<li><a href='#'>Sub Product</a></li>
+						<li><a href='#'>Sub Product</a></li>
+					 </ul>
+				  </li>
+			   </ul>
+			</li>
+			<li><a href='#'>Support</a></li>
+			<li><a href='#'>Media Center</a></li>
+			<li><a href='#'>Contact Us</a></li>
+		</ul>
+</nav>
+	</div>
 </div>
