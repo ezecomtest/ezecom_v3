089d2f00f74948bec4b8764eb1b5fc96137ec5da
chan.raksmey <chan.raksmey@ezecomcorp.com>
2018-02-13 11:50:12 +0700

    13.02.2018 Menu
    13.02.2018 Menu

--- ezecom/ezeapplication/ezecom/views/header/main_nav_user_v.php
+++ ezecom/ezeapplication/ezecom/views/header/main_nav_user_v.php
@@ -1,5 +1,6 @@
 <!-- Main Navigation import CSS-->
 <link href="<?php echo base_url()?>assets/css/css_v3/navigation_menu.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- Main Navigation import JS-->
 <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/js/js_v3/nav_dropdown_menu.js"></script>
@@ -7,31 +8,79 @@
 <div class="container-fluid" style="background-color:#BCBDC0">
 	<div class="container">
 		<nav id='cssmenu'>
-		<div id="head-mobile"></div>
-		<div class="button"></div>
-		<ul>
-			<li class='active'><a href='#'>Home</a></li>
-			<li><a href='#'>Our Company</a></li>
-			<li><a href='#'>Our Services</a>
-			   <ul>
-				  <li><a href='#'>Product 1</a>
-					 <ul>
-						<li><a href='#'>Sub Product</a></li>
-						<li><a href='#'>Sub Product</a></li>
-					 </ul>
-				  </li>
-				  <li><a href='#'>Product 2</a>
-					 <ul>
-						<li><a href='#'>Sub Product</a></li>
-						<li><a href='#'>Sub Product</a></li>
-					 </ul>
-				  </li>
-			   </ul>
-			</li>
-			<li><a href='#'>Support</a></li>
-			<li><a href='#'>Media Center</a></li>
-			<li><a href='#'>Contact Us</a></li>
-		</ul>
-</nav>
+			<div id="head-mobile"></div>
+			<div class="button"></div>
+			<ul>
+				<li class='active'><a href='#'>Home</a></li>
+				<li><a href='#'>Our Company <i class="fa fa-angle-double-right"></i></a>
+					<ul>
+						<li><a href='#'>Company Profile</a></li>
+						<li><a href='#'>Quality Management System</a></li>
+						<li><a href='#'>Childsafe Certification</a></li>
+						<li><a href='#'>Our Partnerships</a>
+							<ul>
+								<li><a href='#'>Dedicated Internet Access</a></li>
+								<li><a href='#'>EzeBiz</a></li>
+								<li><a href='#'>EzeSurf</a></li>
+								<li><a href='#'>EzeSurf</a></li>
+							</ul>
+						</li>
+						<li><a href='#'>Corporate Social Responsibility</a></li>
+						<li><a href='#'>Community</a></li>
+						<li><a href='#'>Career Opportunities</a></li>
+					</ul>
+				</li>
+				<li><a href='#'>Our Services <i class="fa fa-angle-double-right"></i></a>
+				   <ul>
+					  <li><a href='#'>Internet Access</a>
+						 <ul>
+							<li><a href='#'>Dedicated Internet Access</a></li>
+							<li><a href='#'>EzeBiz</a></li>
+							<li><a href='#'>EzeSurf</a></li>
+							<li><a href='#'>EzeSurf</a></li>
+						 </ul>
+					  </li>
+					  <li><a href='#'>Enterprise Network</a>
+						 <ul>
+							<li><a href='#'>IPLC</a></li>
+							<li><a href='#'>DPLC</a></li>
+							<li><a href='#'>MPLS</a></li>
+							<li><a href='#'>VPLS</a></li>
+						 </ul>
+					  </li>
+					  <li><a href='#'>Cloud Services</a>
+						 <ul>
+							<li><a href='#'>Microsoft Cloud Service</a></li>
+							<li><a href='#'>Amazon Web Service</a></li>
+						 </ul>
+					  </li>
+					  <li><a href='#'>Data Center & Hosting </a>
+						 <ul>
+							<li><a href='#'>Colocation</a></li>
+							<li><a href='#'>DRC</a></li>
+							<li><a href='#'>Web Hosting</a></li>
+							<li><a href='#'>VPBX</a></li>
+						 </ul>
+					  </li>
+					  <li><a href='#'>Cyber Security</a>
+						 <ul>
+							<li><a href='#'>DDoS Mitigation Service</a></li>
+							<li><a href='#'>Web Application Firewall</a></li>
+							<li><a href='#'>Managed Network Security</a></li>
+						 </ul>
+					  </li>
+					  <li><a href='#'>Value Added Services</a>
+						 <ul>
+							<li><a href='#'>VoIP</a></li>
+							<li><a href='#'>EzeTV</a></li>
+						 </ul>
+					  </li>
+				   </ul>
+				</li>
+				<li><a href='#'>Support <i class="fa fa-angle-double-right"></i></a></li>
+				<li><a href='#'>Media Center <i class="fa fa-angle-double-right"></i></a></li>
+				<li><a href='#'>Contact Us</a></li>
+			</ul>
+	</nav>
 	</div>
 </div>
