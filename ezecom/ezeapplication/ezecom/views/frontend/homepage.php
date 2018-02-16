db8be0df13d6e1fa9df3b1be0f73ccb9bd2c9e6f
ezecomtest <32106406+ezecomtest@users.noreply.github.com>
2018-02-17 02:33:05 +0700

    help me choose , sign me up and achevment section
    help me choose , sign me up and achevment section

--- ezecom/ezeapplication/ezecom/views/frontend/homepage.php
+++ ezecom/ezeapplication/ezecom/views/frontend/homepage.php
@@ -178,75 +178,205 @@
 		</div>
 		<div class="col-md-4 col-sm-6 col-xs-12 item-margin-bt">
 			<div class="img-wrapper">
-				<img class="img-responsive" src="<?php echo base_url()?>images/homepage/enterprise-network.jpg" alt="Enterprise Network" title="Enterprise Network"/>
+				<figure>
+					<img class="img-responsive" src="<?php echo base_url()?>images/homepage/enterprise-network.jpg" alt="Enterprise Network" title="Enterprise Network"/>
+				</figure>
+				<div class="overlay-products">
+					<ul>
+						<li><a href="#">IPLC</a><span>|</span></li>
+						<li><a href="#">DPLC</a><span>|</span></li>
+						<li><a href="#">MPLS</a><span>|</span></li>
+						<li><a href="#">VPLS</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-sm-6 col-xs-12 item-margin-bt">
 			<div class="img-wrapper">
-				<img class="img-responsive" src="<?php echo base_url()?>images/homepage/cloud-services.jpg" alt="Cloud Services" title="Cloud Services"/>
+				<figure>
+					<img class="img-responsive" src="<?php echo base_url()?>images/homepage/cloud-services.jpg" alt="Cloud Services" title="Cloud Services"/>
+				</figure>
+				<div class="overlay-products">
+					<ul>
+						<li><a href="#">Microsoft Cloud	Service</a><span>|</span></li>
+						<li><a href="#">Amazon Web Service</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-4 col-sm-6 col-xs-12 item-margin-bt">
 			<div class="img-wrapper">
-				<img class="img-responsive" src="<?php echo base_url()?>images/homepage/data-center.jpg" alt="Data Centre & Hosting" title="Data Centre & Hosting"/>
+				<figure>
+					<img class="img-responsive" src="<?php echo base_url()?>images/homepage/data-center.jpg" alt="Data Centre & Hosting" title="Data Centre & Hosting"/>
+				</figure>
+				<div class="overlay-products">
+					<ul>
+						<li><a href="#">Colocation</a><span>|</span></li>
+						<li><a href="#">DRC</a><span>|</span></li>
+						<li><a href="#">Web Hosting</a><span>|</span></li>
+						<li><a href="#">VPBX</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-sm-6 col-xs-12 item-margin-bt">
 			<div class="img-wrapper">
-				<img class="img-responsive" src="<?php echo base_url()?>images/homepage/cyber-security.jpg" alt="Cyber Security" title="Cyber Security"/>
+				<figure>
+					<img class="img-responsive" src="<?php echo base_url()?>images/homepage/cyber-security.jpg" alt="Cyber Security" title="Cyber Security"/>
+				</figure>
+				<div class="overlay-products">
+					<ul>
+						<li><a href="#">DDoS Mitigation	Service</a><span>|</span></li>
+						<li><a href="#">Web	Application	Firewall</a><span>|</span></li>
+						<li><a href="#">Managed	Network	Security</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="col-md-4 col-sm-6 col-xs-12 item-margin-bt">
 			<div class="img-wrapper">
-				<img class="img-responsive" src="<?php echo base_url()?>images/homepage/add-services.jpg" alt="Value Added Services" title="Value Added Services"/>
+				<figure>
+					<img class="img-responsive" src="<?php echo base_url()?>images/homepage/add-services.jpg" alt="Value Added Services" title="Value Added Services"/>
+				</figure>
+				<div class="overlay-products">
+					<ul>
+						<li><a href="#">Voice Over Internet	Protocol (VoIP)</a><span>|</span></li>
+						<li><a href="#">EzeTV</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-	
-
-
-
-
-
-
-
-
-
-<div class="home">
-<!-- HOME SL 1 -->
-	<!--<div class="wrap t3-sl t3-sl-1 bg-color-gray ">
-		<div class="container">
-				
-
-
-			</div>
-		</div>-->
-
-<div class="wrap t3-sl t3-sl-2 t3-home-2 ">
-
+<!-- Sign Up -->
 <div class="container">
-				<!-- <div id="more-btn">
-	<p><a></a></p>
-</div> -->
+	<div class="row">
+		<div class="col-md-3 col-sm-3 col-xs-2"></div>
+		<div class="col-md-6 col-sm-6 col-xs-8">
+			<div class="wrapper-signme-hlepme">
+				<div id="help_more-btn-over" class="pull-left">
+					<div class="imghelphover"></div>
+					<h3 class="text-center">Help Me Choose</h3>
+					<p class="text-center">Tell us your requirement to get the<br/>
+	best suitable internet package.</p>
+				</div>
+				<div id="more-btn-over" class="pull-right">
+					<div class="imghover"></div>
+					<h3 class="text-center">Sign Me Up</h3>
+					<p class="text-center">Let us know your interest <br/>on our package
+	choice and keep in <br/>touch with our latest update.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-3 col-xs-2"></div>
+	</div>
+</div>
+<!-- Looking for -->
+<div class="container bg-lookingfor">
+	<div class="row">
+		<div class="col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="col-md-10 col-sm-10 col-xs-10">
+			<div class="wrapper-looking">
+				<h3 class="text-center">What are you looking for?</h3>
+				<p class="text-center">In this section you can access to some 
+				important part of the whole website by mouce click away.</p>
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-xs-12">
+						<div class="wrapper-news">
+							<a href="#">
+								<img class="img-responsive center-block" src="<?php echo base_url()?>images/homepage/news-event.png" alt="News and Events" title="News and Events"/>
+							</a>
+							<h3 class="text-center">News and Events</h3>
+							<p class="text-center">
+								Check this section for the latest on 
+								EZECOM events. Our sponsorship 
+								and our latest partnerships.
+							</p>
+						</div>
+						
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12">
+						<div class="wrapper-traffic-live">
+							<a href="#">
+								<img class="img-responsive center-block" src="<?php echo base_url()?>images/homepage/live-traffic-cam.png" alt="Live Traffic Camera" title="Live Traffic Camera"/>
+							</a>
+							<h3 class="text-center">Live Traﬃc Camera</h3>
+							<p class="text-center">
+								Always know what is happening.
+								Thanks to EZECOM's traﬃc
+								camera in Phnom Penh.
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-12">
+						<div class="wrapper-live-radio">
+							<a href="#">
+								<img class="img-responsive center-block" src="<?php echo base_url()?>images/homepage/live-radio.png" alt="Live Radio" title="Live Radio"/>
+							</a>
+							<h3 class="text-center">Live Radio</h3>
+							<p class="text-center">
+								Click here to listen to your
+								best favorite radio stations
+								live stream in Cambodia.
+							</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			
+		</div>
+		<div class="col-md-1 col-sm-1 col-xs-1"></div>
+	</div>
+</div>
+<!-- Our Achievement -->
+<div class="container">
+	<div class="row">
+		<div class="col-md-1 col-sm-1 col-xs-1"></div>
+		<div class="col-md-10 col-sm-10 col-xs-10">
+			<div class="wrapper-achievement">
+				<h3 class="text-center">Our Achievement</h3>
+				<p class="text-center">
+					In this section you can access to some 
+					important part of the whole 
+					website by mouce click away.
+				</p>
+				<div class="row">
+					<div class="col-md-3 col-sm-3 col-xs-12">
+						<img class="img-responsive img-award-margin" src="<?php echo base_url()?>images/homepage/best-imployer-award.jpg" alt="Best Imployer Award" title="Best Employer Award"/>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12">
+						<img class="img-responsive img-award-margin" src="<?php echo base_url()?>images/homepage/ISO-Certified.jpg" alt="ISO Certified" title="ISO Certified"/>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12">
+						<img class="img-responsive img-award-margin" src="<?php echo base_url()?>images/homepage/childsafe.jpg" alt="ChilSafe Certified" title="ChilSafe Certified"/>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-12">
+						<img class="img-responsive img-award-margin" src="<?php echo base_url()?>images/homepage/best-ISP.jpg" alt="Best ISP For Business" title="Best ISP For Business"/>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="col-md-1 col-sm-1 col-xs-1"></div>
+	</div>
+</div>
+
+<!-- HTML Form Help me choose and sign me up -->
+<div class="home">
+<div class="wrap t3-sl t3-sl-2 t3-home-2 ">
+<div class="container">
 <div class="col-xs-8 col-sm-8">
 	<div id="moremenu" class="morehover">
-	   <!--  <div class="top" id="more-btn-over">
-	    	<p></p>
-	    </div> -->
-	    <div class="mid">
-	    	<br/>
-	    	
-	          <FORM id="formsignup_form" name="signup" class="thickbox" title="Nice title" method="post">
+	    <div class="mid">	
+	          <form id="formsignup_form" name="signup" class="thickbox" title="Nice title" method="post">
 	            <div class="col-xs-7 col-sm-6">
 		           <p>Which packages interest you:</p>
 		           <div class="packagebox">
 		               <h5>Corporate</h5>
 		               <table border="0" cellspacing="0" cellpadding="0">
-		                  
-		                    
 		                    <tr>
 		                        <td><input class="chkbox" type="checkbox" name="package" value="1mbps $59 per month" id="corp1mbps"  /></td>
 		                        <td><label for="corp1mbps"><strong>1mbps</strong> $40 per month</label></td>
@@ -318,7 +448,7 @@
 		           </div>
 		       </div>
 		      
-	          </FORM>
+	          </form>
 	    	
 	        <br/>
 	    </div>
@@ -610,119 +740,11 @@
 </script>
 
 
-<div class="custom"  >
-	<div class="row" style="margin-top: -50px;">
-<div class="col-sm-12 col-md-6 col-xs-12"><img class="img-responsive" src="images/homepage/lady_new.png" alt="Ezecom Call to Action" width="250" height="301" /></div>
-<div class="col-sm-12 col-md-6 col-xs-12" style="margin-top: -90px;">
-<div class="col-sm-12 col-md-6 col-xs-12">
-<div class="col-xs-12 col-sm-12 col-md-12">
-<blockquote>
-<h3> </h3>
-</blockquote>
- </div>
-<div id="help_more-btn-over" class="help_top"><br /><br /><br /><br /><br /><br /><br />
-<div class="imghelphover"> </div>
-</div>
-<h3>Help Me Choose</h3>
-<p style="font-size: 14px; color: #444;">Tell us your requirement to get the best suitable internet package.</p>
-</div>
-<div class="col-sm-12 col-md-6 col-xs-12">
-<div class="col-xs-12 col-sm-12 col-md-12">
-<blockquote>
-<h3> </h3>
-</blockquote>
- </div>
-<div id="more-btn-over" class="top"><br /><br /><br /><br /><br /><br /><br />
-<div class="imghover"> </div>
-</div>
-<h3>Sign Me Up</h3>
-<p style="font-size: 14px; color: #444;">Let us know your interest on our package choice and keep in touch with our latest update.</p>
-</div>
-</div>
-</div></div>
-
 			</div>
 		</div>
-		<!-- //HOME SL 2 -->
-	        <section class="t3-home-3">       
-			<!-- HOME SL 3 -->
-		<div class="wrap t3-sl t3-sl-3 ">
-			<div class="container">
-				
-
-<div class="custom"  >
-	<div style="height: 20px; width: 100%;"> </div>
-<div class="container-fluid">
-<div class="col-xs-12 col-sm-6 col-md-4"><a href="<?php echo base_url();?>mediacenter">
-<div class="newandeventhover"> </div>
-<h3>News and Events</h3>
-<p style="font-size: 14px; color: #444;">Check this section for the latest on EZECOM events. Our sponsorship and our latest partnerships.</p></a>
-</div>
-<div class="col-xs-12 col-sm-6 col-md-4"><a href="<?php echo base_url();?>cameralive">
-<div class="livecamerahover"> </div>
-<h3>Live Traffic Camera</h3>
-<p style="font-size: 14px; color: #444;">Always know what is happening. Thanks to EZECOM's traffic camera in Phnom Penh.</p></a>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-4"><a href="<?php echo base_url();?>liveradio">
-<div class="liveradiohover"> </div>
-<h3>Live Radio</h3>
-<p style="font-size: 14px; color: #444;">Click here to listen to your best favorite radio stations live stream in Cambodia.</p></a>
-</div>
-</div>
-<div style="height: 20px; width: 100%;"> </div></div>
-
-			</div>
-		</div>
-		<!-- //HOME SL 3 -->
-	        </section>
-			<!-- HOME SL 4 -->
-		<div class="wrap t3-sl t3-sl-4 ">
-			<div class="container">
-				
-
-<div class="custom"  >
-	<div class="custom" style="margin-top: -31px;">
-<div class="container module-content-container">
-<h2 style="color: #7c5ca8; font-weight: 400;"> Our Services</h2>
-<div class="col-xs-12 col-sm-12 col-md-2"> </div>
-<div class="col-xs-12 col-sm-12 col-md-8">
-<p style="font-size: 14px !important; color: #444; line-height: 25px;">EZECOM offers you the best available packages to fulfill your needs, from specially designed packages for your resident to international dedicated connections and your global enterprise solutions - click here for more packages information.</p>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-4"> </div>
-<br />
-<div class="col-xs-12 col-sm-12 col-md-12"> 
-<div class="col-xs-12 col-sm-12 col-md-2"> </div>
-<div class="col-xs-12 col-sm-12 col-md-4"><a href="<?php echo base_url();?>ourservices">
-<div class="ourinternethover"></div>
-<h3>Our Internet</a></h3>
-<p style="font-size: 14px; color: #444444;"> </p></a>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-4"><a href="<?php echo base_url();?>enterprise-solution">
-<div class="enterprisesolutions"> </div>
-<h3>Enterprise Solutions</h3>
-<p style="font-size: 14px; color: #444444;"> </p></a>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-2"> </div>
-</div>
-</div>
-</div></div>
-
-			</div>
-
-		</div>
-		<!-- //HOME SL 4 -->
-	
-			<!-- HOME SL 5 -->
-		<div class="wrap t3-sl t3-sl-5 ">
-			<marquee onmouseover="this.stop();" onmouseout="this.start();" scrollamount="12">
-				<?php foreach($feature_content as $feature){ ?>
-					<a target="_blank" href="<?php echo base_url('newsdetail/').strtolower(str_replace(' ', '-' ,$feature->content_title)) ?>"><?php echo $feature->content_title.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' ?></a>
-				<?php } ?>
-			</marquee>
-
-		</div>
-		<!-- //HOME SL 5 -->
-	
+		
+		
+		
 </div>
 
 <!-- FOOTER -->
