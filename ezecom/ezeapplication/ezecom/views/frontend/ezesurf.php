1c701977597b6d1e631f823df040380a5c6c60f5
chan.raksmey <chan.raksmey@ezecomcorp.com>
2018-02-28 08:46:51 +0700

    update style term condition on Ezesurf
    update style term condition on Ezesurf

--- ezecom/ezeapplication/ezecom/views/frontend/ezesurf.php
+++ ezecom/ezeapplication/ezecom/views/frontend/ezesurf.php
@@ -133,7 +133,7 @@
 		</div>
 		<div class="row mg-bt">
 			<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-xs-2 "></div>
-			<div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+			<div class="col-xl-8 col-lg-8 col-md-8 col-sm-10 col-xs-10 ">
 				<div class="term-condition">
 					<p>TERMS & CONDITIONS</p>
 					<ul>
