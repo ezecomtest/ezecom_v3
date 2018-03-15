3c8c9f45b0bd683f590a5cb285f4a3827cda047a
chan.raksmey <chan.raksmey@ezecomcorp.com>
2018-03-15 09:42:29 +0700

    customer service done
    customer service done

--- ezecom/ezeapplication/ezecom/controllers/frontend/Customer_service_c.php
+++ ezecom/ezeapplication/ezecom/controllers/frontend/Customer_service_c.php
@@ -71,26 +71,37 @@ class Customer_service_c extends CI_Controller {
 	}
 	
 	public function customer_services(){
+		
 		if($this->session->userdata("language")==1){
-		$data['title'] = "Customer Services";
-		$data['active'] = "Customer Service";
-		$lan = $this->session->userdata("language");
-		$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
-		$this->load->view('frontend/customer_service_v',$data);
+			$data['title'] = "Customer Services";
+			$data['active'] = "Customer Service";
+			$lan = $this->session->userdata("language");
+			$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
+			$this->load->view('frontend/customer_service_v',$data);
 		}
-		if($this->session->userdata("language")==2){
-		$data['title'] = "សេវាអតិថិជន";
-		$data['active'] = "Customer Service";
-		$lan = $this->session->userdata("language");
-		$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
-		$this->load->view('frontend/customer_service_kh_v ',$data);
+		
+		/* if($this->session->userdata("language")==2){
+			$data['title'] = "សេវាអតិថិជន";
+			$data['active'] = "Customer Service";
+			$lan = $this->session->userdata("language");
+			$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
+			$this->load->view('frontend/customer_service_kh_v ',$data);
 		}
 		if($this->session->userdata("language")==3){
-		$data['title'] = "Customer Services";
-		$data['active'] = "Customer Service";
-		$lan = $this->session->userdata("language");
-		$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
-		$this->load->view('frontend/customer_service_ch_v',$data);
+			$data['title'] = "Customer Services";
+			$data['active'] = "Customer Service";
+			$lan = $this->session->userdata("language");
+			$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
+			$this->load->view('frontend/customer_service_ch_v',$data);
+		} */
+		
+		if($this->session->userdata("language")==""){
+			$data['title'] = "Customer Services";
+			$data['active'] = "Customer Service";
+			$lan = 1;
+			$data['feature_content'] = $this->homepage_m->get_feature_content($lan);
+			$this->load->view('frontend/customer_service_v',$data);
 		}
+		
 	}
 }
